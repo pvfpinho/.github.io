3a2b556aad64c12a7688b965148940d22b45454c
pvfpinho <pvfpinho@hotmail.com>
2020-12-16 19:22:16 -0700

    lesson12

--- lesson12/js/price.js
+++ lesson12/js/price.js
@@ -7,7 +7,7 @@ fetch(rentalsURL)
 
      
 
-     document.getElementById('scooters').innerHTML = 'Scoots offers three types of Honda motor scooters, the Metro, Dio, and the PCX150.' + 'The Metro reserved is $' +
+     document.getElementById('scooters').innerHTML = 'Types of Honda motor scooters, the Metro, Dio, and the PCX150.' + 'The Metro reserved is $' +
       rentalObj.rentals[0].resHalfday + ' for half a day or for a full day $' + rentalObj.rentals[0].resFullday + '.' + 'The walk-in price is $' + rentalObj.rentals[0].walkHalfday + ' for half a day and $' +rentalObj.rentals[0].walkFullday 
       + ' for a full day.' + 'The Dio reserved is $' + rentalObj.rentals[1].resHalfday + ' half day or $' + rentalObj.rentals[1].resFullday + ' full day.' 
       + 'Walk-in it is $' + rentalObj.rentals[1].walkHalfday + ' half day or $' + rentalObj.rentals[1].walkFullday + ' full day.' + 
@@ -19,7 +19,7 @@ fetch(rentalsURL)
       + rentalObj.rentals[3].walkHalfday + ' or for a full day it is $' + rentalObj.rentals[3].walkFullday + '.';
 
       document.getElementById('jeeps').innerHTML = 'Scoots offers two different options of Jeep Wrangler; four door manual with a/c or two door manual open air. The four door reserved is $' + 
-      rentalObj.rentals[4].resHalfday + ' for half a day, or for a full day $' + rentalObj.rentals[4].resFullday + '. For walk-in half day it is $' + rentalObj.rentals[4].walkHalfday + '. Full day costs $' + rentalObj.rentals[4].walkFullday + 
+      rentalObj.rentals[4].resHalfday + ' for half a day, or for a full day $' +  rentalObj.rentals[4].resFullday +  '. For walk-in half day it is $' + rentalObj.rentals[4].walkHalfday + '. Full day costs $' + rentalObj.rentals[4].walkFullday + 
       '. The two door reserved for half a day is $' + rentalObj.rentals[5].resHalfday + ' or a full day is $' + rentalObj.rentals[5].resFullday + 
       '. For walk-in is costs $' + rentalObj.rentals[5].walkHalfday + ' for half a day and $' + rentalObj.rentals[5].walkFullday + ' for a full day.';
   });
